29bff92c9cef73b45996e087cf280463c8a180ca
Amulya Shrivastava <149600311+Always-Amulya7@users.noreply.github.com>
2025-08-27 19:31:38 +0530

    Update solution: Roman to Integer (LcSoln)

--- LeetCode/Easy/Roman to Integer/Roman_to_Integer.java
+++ LeetCode/Easy/Roman to Integer/Roman_to_Integer.java
@@ -1,5 +1,25 @@
 class Solution {
     public int romanToInt(String s) {
-        
+        int Number = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1) {
+                if (getValue(s.charAt(i)) < getValue(s.charAt(i + 1))) {
+                    Number -= getValue(s.charAt(i));
+                    continue;
+                }
+            }
+            Number += getValue(s.charAt(i));
+        }
+        return Number;
+    }
+    
+    private int getValue(char c) {
+        if (c == 'I') return 1;
+        else if (c == 'V') return 5;
+        else if (c == 'X') return 10;
+        else if (c == 'L') return 50;
+        else if (c == 'C') return 100;
+        else if (c == 'D') return 500;
+        else return 1000;
     }
 }
